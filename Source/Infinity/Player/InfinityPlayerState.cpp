cf04dfd92a19f5acb2c60ad7a3f9d275597c7034
King2481 <bjcrum94@gmail.com>
2020-07-28 19:02:00 -0500

    Transfered overed MatchStats from Arena to Infinity

--- Source/Infinity/Player/InfinityPlayerState.cpp
+++ Source/Infinity/Player/InfinityPlayerState.cpp
@@ -2,8 +2,37 @@
 
 
 #include "InfinityPlayerState.h"
+#include "Net/UnrealNetwork.h"
 
 AInfinityPlayerState::AInfinityPlayerState()
 {
 
+}
+
+void AInfinityPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate to everyone
+	DOREPLIFETIME(AInfinityPlayerState, MatchStats);
+}
+
+FMatchStats AInfinityPlayerState::GetMatchStats() const
+{
+	return MatchStats;
+}
+
+void AInfinityPlayerState::ScoreKill(int32 Amount)
+{
+	MatchStats.Kills += Amount;
+}
+
+void AInfinityPlayerState::ScoreAssist(int32 Amount)
+{
+	MatchStats.Assists += Amount;
+}
+
+void AInfinityPlayerState::ScoreDeath(int32 Amount)
+{
+	MatchStats.Deaths += Amount;
 }
--- Source/Infinity/Player/InfinityPlayerState.h
+++ Source/Infinity/Player/InfinityPlayerState.h
@@ -6,6 +6,31 @@
 #include "GameFramework/PlayerState.h"
 #include "InfinityPlayerState.generated.h"
 
+USTRUCT(BlueprintType)
+struct FMatchStats
+{
+	GENERATED_BODY()
+
+	// Kills Scored
+	UPROPERTY(BlueprintReadOnly)
+	int32 Kills;
+
+	// Assists Scored
+	UPROPERTY(BlueprintReadOnly)
+	int32 Assists;
+
+	// Deaths Scored
+	UPROPERTY(BlueprintReadOnly)
+	int32 Deaths;
+
+	FMatchStats()
+	{
+		Kills = 0;
+		Assists = 0;
+		Deaths = 0;
+	}
+};
+
 /**
  * 
  */
@@ -17,5 +42,27 @@ class INFINITY_API AInfinityPlayerState : public APlayerState
 public:
 
 	AInfinityPlayerState();
+
+	// Returns the match statss for the current match
+	UFUNCTION(BlueprintPure, Category = "Player State")
+	FMatchStats GetMatchStats() const;
+
+	// Scores a death, must be called on auth to replicate.
+	void ScoreKill(int32 Amount = 1);
+
+	// Scores a death, must be called on auth to replicate.
+	void ScoreAssist(int32 Amount = 1);
+
+	// Scores a death, must be called on auth to replicate.
+	void ScoreDeath(int32 Amount = 1);
+
+protected:
+
+	// Function for replication setup.
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// Match stats for this player.
+	UPROPERTY(BlueprintReadOnly, Replicated, Category = "Player State")
+	FMatchStats MatchStats;
 	
 };
