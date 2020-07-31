4879ea25fbe1fc8a25b379bd42a4950626b20c7b
King2481 <bjcrum94@gmail.com>
2020-07-31 17:35:50 -0500

    Added InfinityDamafgeType (we will eventually need this)

---
+++ Source/Infinity/Weapons/InfinityDamageType.h
@@ -0,0 +1,21 @@
+// Made by Bruce Crum
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "InfinityDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINITY_API UInfinityDamageType : public UDamageType
+{
+	GENERATED_BODY()
+
+public:
+
+	UInfinityDamageType();
+	
+};
